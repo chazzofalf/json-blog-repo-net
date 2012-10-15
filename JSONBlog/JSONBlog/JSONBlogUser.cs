1fe6a61d7edc4e351eb127cd7157e8d3deee71a2
Charles Montgomery <charles.montgomery@charter.net>
2012-10-15 11:48:34 -0500

    Added Newtonsoft JSON Library Working on JSONBlogUser Authentication

--- JSONBlog/JSONBlog/JSONBlogUser.cs
+++ JSONBlog/JSONBlog/JSONBlogUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,11 @@ using System.IO;
 using System.Security.Cryptography;
 namespace JSONBlog
 {
+    /// <summary>
+    /// Represents a registered user of this blog system.
+    /// 
+    /// </summary>
+    /// 
     [Serializable()]
     class JSONBlogUser
     {
@@ -21,19 +27,81 @@ namespace JSONBlog
         {
             get
             {
+                return passHash;
             }
             set
+            {
+                byte[] salt = generateSalt();
+                byte[] passw = passwordToHash(value);
+                int pos = randomPosition();
+                placeHashInSalt(passw, salt, pos);
+                position = pos;
+                passHash = Convert.ToBase64String(salt);
+            }
+        }
+        public bool LoginSuccessful(string password)
+        {
+            byte[] hash = passwordToHash(password);
+            byte[] passHash = Convert.FromBase64String(this.passHash);
+            byte[] storedhash = new byte[hash.Length];
+            if (position < hash.Length)
+            {
+                Array.Copy(passHash, position, storedhash, 0, storedhash.Length);
+            }
+            else
             {
 
             }
         }
-        private string passwordToHash(string password)
+        private byte[] passwordToHash(string password)
         {
             SHA512Managed sha512 = new SHA512Managed();
-             
-
+            MemoryStream passwordStore = new MemoryStream();
+            TextWriter passwordWriter = new StreamWriter(passwordStore, Encoding.UTF32);
+            passwordWriter.Write(password);
+            byte[] hash = sha512.ComputeHash(passwordStore.ToArray());
+            return hash;
         }
-
+        private byte[] generateSalt()
+        {
+            byte[] salt = new byte[1024/8];
+            RNGCryptoServiceProvider.Create().GetBytes(salt);
+            return salt;
+        }
+        private int randomPosition()
+        {
+            byte[] intPick = new byte[4];
+            RNGCryptoServiceProvider.Create().GetBytes(intPick);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(intPick);
+            }
+            int pick = BitConverter.ToInt32(intPick, 0);
+            pick %= 128;
+            return pick;
+        }
+        private void placeHashInSalt(byte[] hash, byte[] salt, int position)
+        {
+            if (position > salt.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (position < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (position > hash.Length)
+            {
+                int firstPartLength = position - hash.Length;
+                int lastPartLength = hash.Length - firstPartLength;
+                Array.Copy(hash, 0, salt, position, firstPartLength);
+                Array.Copy(hash, firstPartLength, salt, 0, lastPartLength);
+            }
+            else
+            {
+                Array.Copy(hash, 0, salt, 0, hash.Length);
+            }
+        }
         public string Username
         {
             get
